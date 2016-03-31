bf0d336b797491877f030dd824b66512b91a3f6d
agory <gory.alexandre02@gmail.com>
2016-03-31 11:59:47 +0200

    Improved Map class

--- Othello_model/Map.cs
+++ Othello_model/Map.cs
@@ -16,15 +16,61 @@ namespace Othello_model
 
         public Map() {
             this.matrix = new int[8, 8];
+            this.matrix[3, 3] = 1;
             this.matrix[4, 4] = 1;
-            this.matrix[5, 5] = 1;
-            this.matrix[4, 5] = -1;
-            this.matrix[5, 4] = -1;
+            this.matrix[3, 4] = -1;
+            this.matrix[4, 3] = -1;
         }
 
         public List<int[]> findMove(int playerValue) {
-
+            for (int l = 0; l < 8; l++)
+            {
+                for (int c = 0; l < 8; c++)
+                {
+                    
+                }
+            }
             return null;
+        }
+
+        private bool spacePlayable(int[] vecteur) {
+            if (matrix[vecteur[0], vecteur[1]] != 0) {
+                return false;
+            }
+
+
+            return true;//testLine(vecteur,
+        }
+
+        private bool testLine(int[] vecteur, int[] direction, int value) {
+            int nextX = vecteur[0] + direction[0];
+            int nextY = vecteur[1] + direction[1];
+            bool test = validPoint(nextX, nextY);
+            if (test & matrix[nextX, nextY] == value * -1) {                
+                while (true) {
+                    nextX = nextX + direction[0];
+                    nextY = nextX + direction[1];
+                    if (!validPoint(nextX, nextY)){
+                        if (matrix[nextX, nextY] == value) {
+                            return false;
+                        }
+                        if (matrix[nextX, nextY] == 0)
+                        {
+                            return true;
+                        }
+                    } else {
+                        return false;
+                    }
+                }
+            }
+            return false;
+        }
+
+        private bool validPoint(int x, int y) {
+            if ((x >= 0 & x < 8) & (y >= 0 & x < 8)) {
+                return true;
+            }
+            return false;
         }
 
         public void playMove(int value, int x, int y) {
