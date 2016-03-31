273f5d24f7f6cb49cb36fb45a9bf0d15aebc470f
agory <gory.alexandre02@gmail.com>
2016-03-31 10:57:04 +0200

    Add lib model\n Add object Map and IA

---
+++ Othello_model/IA.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Othello_model
+{
+    public class IA
+    {
+        public IA() {
+
+        }
+
+        public void play(Map map) {
+            // Method de jeu
+        }
+    }
+}
---
+++ Othello_model/Map.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Othello_model
+{
+    public class Map
+    {
+        private int[,] matrix;
+
+        public Map(int[,] matrix) {
+            this.matrix = matrix;
+        }
+
+        public Map() {
+            this.matrix = new int[8, 8];
+            this.matrix[4, 4] = 1;
+            this.matrix[5, 5] = 1;
+            this.matrix[4, 5] = -1;
+            this.matrix[5, 4] = -1;
+        }
+
+        public List<int[]> findMove() {
+
+            return null;
+        }
+
+        public void playMove(int value, int x, int y) {
+            matrix[x,y] = value;
+        }
+
+        public int getScore(int value) {
+            int score = 0;
+            for (int l = 0; l < 8; l++) {
+                for (int c = 0; l < 8; c++) {
+                    if (matrix[l,c] == value) {
+                        score++;
+                    }
+                }                
+            }
+            return score;
+        }
+
+        public int[,] getMatrix() {
+            return matrix;
+        }
+
+    }
+}
